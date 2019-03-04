645954eb41ccb7a75b5fe15ce713baab00b1bb3c
Phillip Marsden <philyorkshire@philyorkshire.uk>
2019-03-04 20:11:57 +0000

    fixed the header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,7 +1,17 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import './header.scss';
 
 class Header extends Component {
+
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			mobileNavState: ''
+		}
+	}
+
     render() {
         return (
 			<header>
@@ -9,12 +19,12 @@ class Header extends Component {
 					<h1 className="logo">Philyorkshire Ltd.</h1>
 				</Link>
 
-				<div className="mobile-toggle">
-					<i className="fa fa-bars" aria-hidden="true"></i>
+				<div className={`mobile-toggle ${this.state.mobileNavState}`}>
+					<i className="fa fa-bars" aria-hidden="true" onClick={this.onClickMobileNavigation}></i>
 				</div>
 
-				<nav className="main-nav">
-					<ul>
+				<nav className={`main-nav ${this.state.mobileNavState}`}>
+					<ul onClick={this.closeMobileNavigation}>
 						<li><Link to="/about">About</Link></li>
 						<li><Link to="/work">Work</Link></li>
 						<li><Link to="/contact">Contact</Link></li>
@@ -22,7 +32,21 @@ class Header extends Component {
 				</nav>
 			</header>
         );
-    }
+	}
+	
+	onClickMobileNavigation = () => {
+		const mobileNavState = this.state.mobileNavState === 'active' ? '' : 'active';
+
+		this.setState({
+			mobileNavState: mobileNavState
+		});
+	}
+
+	closeMobileNavigation = () => {
+		this.setState({
+			mobileNavState: ''
+		});
+	}
 }
 
 export default Header;
